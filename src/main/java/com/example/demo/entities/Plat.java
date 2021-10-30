d7d2342e47d56a3b7c653b055da8c2f1a337f964
chermrone <marouane.cherif1@esprit.tn>
2021-10-30 01:20:46 +0100

    sqlite ok

--- src/main/java/com/example/demo/entities/Plat.java
+++ src/main/java/com/example/demo/entities/Plat.java
@@ -1,6 +1,11 @@
 package com.example.demo.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Plat {
+	@Id
 	private Long id;
 	private String nom;
 	private double prix;
